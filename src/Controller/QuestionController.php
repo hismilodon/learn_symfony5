69ae34935442bb227d549f6d51915f8c1af26451
hismilodon <hismilodon@localhost>
2020-07-06 00:30:56 +0800

    named autowiring

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -3,11 +3,9 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
-use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
-use Symfony\Contracts\Cache\CacheInterface;
+use App\Service\MarkdownHelper;
 
 class QuestionController extends AbstractController
 {
@@ -22,19 +20,19 @@ class QuestionController extends AbstractController
   /**
    * @Route("/questions/{slug}", name="app_question_show")
    */
-  public function show($slug, MarkdownParserInterface $markdownParser, CacheInterface $cache)
+  public function show($slug, MarkdownHelper $markdownHelper)
   {
+    dump($this->getParameter('cache_adapter'));
+    
     $answers = [
       'Make sure your cat is sitting `purrrfectly` still 🤣',
       'Honestly, I like furry shoes better than MY cat',
       'Maybe... try saying the spell backwards?',
     ];
     $questionText = 'I\'ve been turned into **a cat**, any thoughts on how to turn back? While I\'m **adorable**, I don\'t really care for cat food.';
-    $parsedQuestionText = $cache->get('markdown_'.md5($questionText), function() use ($questionText, $markdownParser) {
-      return $markdownParser->transformMarkdown($questionText);
-    });
+    $parsedQuestionText = $markdownHelper->parse($questionText);
 
-    dump($cache);
+    dump($markdownHelper);
 
     return $this->render('question/show.html.twig', [
       'question' => ucwords(str_replace('-', ' ', $slug)),
